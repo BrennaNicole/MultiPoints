ea3a0fa208208bb13e4b8a72d2d80dc573300015
BrennaNicole <BrieAnna.Allen@loop.colum.edu>
2018-09-30 22:42:40 -0500

    Add files via upload

---
+++ MultiPoints/MultiPoints.js
@@ -0,0 +1,92 @@
+﻿var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '   gl_Position = a_Position;\n' +
+    '   gl_PointSize = 10.0;\n' +
+    '}\n';
+
+var FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'uniform vec4 u_FragColor;\n' +
+    'void main() {\n' +
+    '   gl_FragColor = u_FragColor;\n' +
+    '}\n';
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to Initialize shaders');
+        return;
+    }
+
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the verticies');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, n);
+
+   /* var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    if (a_Position < 0) {
+        console.log('Failed to get storage location for a_Position');
+        return;
+    }
+
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (u_FragColor < 0) {
+        conso.log('Failed to get storage location for u_FragColor');
+        return;
+    }
+
+    canvas.onmousedown = function (ev) { click(ev, gl, canvas, a_Position, u_FragColor); };
+
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+  
+
+        g_colors.push([1.0, 1.0, 1.0, 1.0]);*/
+    }
+
+
+function initVertexBuffers(gl) {
+    var verticies = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+
+    var n = 3;
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    gl.bufferData(gl.ARRAY_BUFFER, verticies, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get storage location for a_Position');
+        return;
+    }
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
+
